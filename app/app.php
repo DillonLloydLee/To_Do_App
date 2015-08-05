ba9bbcf758504fd6d396baba52244d589c85085c
Kyle Pratuch & Dillon Lee <student@epicodus.com>
2015-08-05 10:54:57 -0700

    Add erase list function.

--- app/app.php
+++ app/app.php
@@ -36,6 +36,9 @@
 
                 <button type='submit'>Add task</button>
             </form>
+            <form action='/delete_tasks' method='post'>
+                <button type='submit'>Remove all tasks!</button
+            </form>
             ";
 
         return $output;
@@ -50,6 +53,16 @@
             <p>" . $task->getDescription() . "</p>
             <p><a href='/'>View your list of things to do.</a></p>
             ";
+    });
+
+    $app->post("/delete_tasks", function() {
+
+        Task::deleteAll();
+
+        return "
+            <h1>List Cleared!</h1>
+            <p><a href='/'>Home</a></p>
+        ";
     });
 
     return $app;
